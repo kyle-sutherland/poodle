117ca877111ecd834ff1188369ac75235359e89a
velveteen-rever <velveteen.rever@gmail.com>
2023-07-28 11:38:34 -0600

    discarded stream manager approach

--- Singleton.py
+++
@@ -1,7 +0,0 @@
-class Singleton:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if not isinstance(cls._instance, cls):
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
-        return cls._instance
--- audio_queue_fetcher.py
+++ audio_queue_fetcher.py
@@ -1,29 +1,43 @@
 import threading
 import time
 import wave
-import pyaudio
-import config
-from audio_stream_manager import AudioStreamManager
 
-asm = AudioStreamManager()
+import pyaudio
+
+import config
 
 
 class AudioQueueFetcher(threading.Thread):
-    def __init__(self, audio_queue, running):
+    def __init__(self, audio_queue, running, channels=config.PYAUDIO_CHANNELS, frames_per_buffer=8000):
         threading.Thread.__init__(self)
         self.audio_queue = audio_queue
         self.running = running
-        self.frames_per_buffer = 8000
-        self.default_device_info = asm.input_device_info
+
+        self.pa = pyaudio.PyAudio()
+        self.channels = channels
+        self.frames_per_buffer = frames_per_buffer
+        self.default_device_info = self.pa.get_default_input_device_info()
         self.sample_rate = int(self.default_device_info['defaultSampleRate'])
-        self.channels = 1
-        self.stream = asm.open_input_stream(1, self.sample_rate, self.frames_per_buffer)
+
+    def dump_audio(self, data, filename="audio_dumps/dump.raw"):
+        with wave.open(filename, "wb") as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+            wf.setframerate(self.sample_rate)
+            wf.writeframes(data)
 
     def run(self):
+        stream = self.pa.open(format=config.PYAUDIO_FORMAT,
+                              channels=config.PYAUDIO_CHANNELS,
+                              rate=self.sample_rate,
+                              input=True,
+                              frames_per_buffer=config.PYAUDIO_FRAMES_PER_BUFFER)
         while self.running.is_set():
-            data = self.stream.read(self.frames_per_buffer)
+            data = stream.read(self.frames_per_buffer)
             self.audio_queue.put((time.time(), data))
-        asm.close_input_stream(self.stream)
+        stream.stop_stream()
+        stream.close()
+        self.pa.terminate()
 
     def stop(self):
         self.running.clear()
