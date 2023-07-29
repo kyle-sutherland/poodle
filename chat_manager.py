c3fefa905841d55c60494216b46d540b5562dd81
velveteen-rever <velveteen.rever@gmail.com>
2023-07-29 12:40:09 -0600

    IT WORKS!!

--- chat_manager.py
+++ chat_manager.py
@@ -1,7 +1,12 @@
 # chat_manager.py
 import os
+import threading
+import time
 
 import openai
+
+import config
+from file_manager import FileManager
 
 ai = openai
 ai.api_key = os.getenv('OPENAI_API_KEY')
@@ -12,17 +17,17 @@ messages = [{"role": "system", "content": "you are a helpful assistant"}]
 
 class ChatSession:
     def __init__(self):
+        self.transcription_directory = config.TRANSCRIPTION_PATH
         self.messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
     def add_user_entry(self, content):
         self.messages.append(
-            {"role": "user", "content": content},
+            {"role": "user", "content": content[0]["text"]},
         )
 
     def add_reply_entry(self, response):
-        if 'choices' in response['model']:
-            reply = response['choices'][0]['message']['content']
-            self.messages.append({"role": "assistant", "content": reply})
+        reply = response['choices'][0]['message']['content']
+        self.messages.append({"role": "assistant", "content": reply})
 
     def send_request(self):
         try:
--- file_manager.py
+++ file_manager.py
@@ -23,27 +23,28 @@ class FileManager:
     @staticmethod
     def mark_as_read(file_name):
         original_file_path = os.path.join(config.TRANSCRIPTION_PATH, file_name)
-        marked_file_path = os.path.join(config.TRANSCRIPTION_PATH, f"read_{file_name}")
+        marked_file_path = os.path.join(config.TRANSCRIPTION_PATH, f"_read_{file_name}")
         os.rename(original_file_path, marked_file_path)
 
     @staticmethod
     def read_transcriptions(directory):
         transcriptions = []
         for file in os.listdir(directory):
-            if file.startswith("_read_"):
-                continue
-            file_path = os.path.join(directory, file)
-            if os.path.exists(file_path):
-                if os.stat(file_path).st_size != 0:
-                    with open(file_path, "r") as f:
-                        try:
-                            transcription = json.load(f)
-                            transcriptions.append(transcription)
-                        except ValueError as e:
-                            print(f"Error reading {file_path}: {e}")
-                    FileManager.mark_as_read(file)
+            if not file.startswith("_read_"):
+                file_path = os.path.join(directory, file)
+                if os.path.exists(file_path):
+                    if os.stat(file_path).st_size != 0:
+                        with open(file_path, "r") as f:
+                            try:
+                                transcription = json.load(f)
+                                transcriptions.append(transcription)
+                            except ValueError as e:
+                                print(f"Error reading {file_path}: {e}")
+                        FileManager.mark_as_read(file)
+                    else:
+                        print(f"File {file_path} is empty.")
                 else:
-                    print(f"File {file_path} is empty.")
+                    print(f"File {file_path} does not exist.")
             else:
-                print(f"File {file_path} does not exist.")
+                continue
         return transcriptions
