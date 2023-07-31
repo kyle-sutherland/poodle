18d6b03010748a965e73bec9139de2bae7364c57
velveteen-rever <velveteen.rever@gmail.com>
2023-07-31 06:03:48 -0600

    performance monitoring. working

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 # main.py
+import gc
+
 import chat_manager
 import kd_listeners
-from audio_utils import KeywordDetector, Transcriber
+from audio_utils import KeywordDetector, Transcriber, TextToSpeech
 from file_manager import FileManager
 import time
 import config
 import event_flags as ef
-
-
-def do_request(chat_session, transcriptions):
-    if len(transcriptions) != 0:
-        chat_session.add_user_entry(transcriptions)
-    resp = chat_session.send_request()
-    chat_session.add_reply_entry(resp)
-    timestamp = FileManager.get_datetime_string()
-    FileManager.save_json(f'{config.RESPONSE_LOG_PATH}response_{timestamp}.json', resp)
-    print(resp["choices"][0]["message"]["content"])
-    ef.silence.clear()
 
 
 def main():
@@ -31,24 +22,41 @@ def main():
 
     if config.ENABLE_ACTIVE_SPEECH_LOG:
         kw_detector.add_partial_listener(kd_listeners.pl_print_active_speech_only)
-
+    ef.speaking.clear()
     chat_session = chat_manager.ChatSession()
     transcriber = Transcriber(config.PATH_PROMPT_BODIES_AUDIO, config.TRANSCRIPTION_PATH)
 
     kw_detector.start()
+
+    def do_request(chat, trans):
+        text_to_speech = TextToSpeech()
+        if len(transcriptions) != 0:
+            chat_session.add_user_entry(transcriptions)
+        resp =  chat_session.send_request()
+        chat_session.add_reply_entry(resp)
+        timestamp = FileManager.get_datetime_string()
+        FileManager.save_json(f'{config.RESPONSE_LOG_PATH}response_{timestamp}.json', resp)
+        kw_detector.close()
+        text_to_speech.make_voice('things')
+        # pyaudio currently seems to have issues in python 3.10. Going to try workarounds on the test branch
+        # text_to_speech.play_voice()
+        text_to_speech.close()
+        kw_detector.start()
+        print(f'{resp["choices"][0]["message"]["content"]}/n')
+        print("")
+        print(f"total response time: {time.time() - ef.stream_write_time} seconds\n")
+        ef.silence.clear()
 
     try:
         while True:
             transcriber.transcribe_bodies()
             if ef.silence.is_set() and not ef.recording.is_set():
                 transcriptions = FileManager.read_transcriptions(config.TRANSCRIPTION_PATH)
-                if len(chat_manager.extract_trans_text(transcriptions)) < 0:
+                if len(chat_manager.extract_trans_text(transcriptions)) == 0:
                     print("I didn't hear you")
                     ef.silence.clear()
                     continue
                 do_request(chat_session, transcriptions)
-                action_time = time.time() - kw_detector.stream_write_time
-                print(f"total response time: {action_time}")
                 convo = chat_session.messages
                 timestamp = FileManager.get_datetime_string()
                 FileManager.save_json(f'{config.CONVERSATIONS_PATH}conversation_{timestamp}.json', convo)
@@ -57,6 +65,7 @@ def main():
         print("Closing the program")
         kw_detector.close()
         kw_detector.join()
+        gc.collect()
 
 
 if __name__ == '__main__':
